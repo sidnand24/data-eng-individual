683e5164ca6d9a28d50ca2a4b94cc712b42f2308
sidnand24 <sidnandalan@gmail.com>
2022-04-22 13:54:42 +0000

    created pre-processed table to be deployed in streamlit

--- ML_salary_prediction.py
+++ ML_salary_prediction.py
@@ -4,6 +4,9 @@ import pyspark
 import findspark
 findspark.init()
 from pyspark.sql import functions as F
+from pyspark.sql.types import IntegerType
+from pyspark.sql.types import DoubleType
+from pyspark.ml.feature import StringIndexer, OneHotEncoder, VectorAssembler
 
 os.environ['PYSPARK_PYTHON'] = sys.executable
 os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
@@ -37,12 +40,13 @@ df_players = spark.read.jdbc(url=url, table="nba.players", properties=properties
 df_stats = spark.read.jdbc(url=url, table="nba.stats", properties=properties)
 df_tweets = spark.read.jdbc(url=url, table="nba.tweets", properties=properties)
 
-# Calculate total number of tweets per player in last week
+# Calculate total tweet count over week per player
 df_tweets = df_tweets.groupBy('player_id').sum('Tweet_count')
 
-# Create final table for machine learning
+# Join tables together to create final ML data for model training
 df_ml = df_players.join(df_tweets, df_players.player_id == df_tweets.player_id).select(df_players["*"], df_tweets["sum(Tweet_count)"])
 
+# Select columns desired for models
 df_ml = df_ml.select(F.col("player_id"), F.col('position'), F.col('age'), F.col('height'), F.col('weight'),
                      F.col('salary'), F.col('sum(tweet_count)').alias('week_tweets'))
 
@@ -68,3 +72,31 @@ def fill_missing(df, exclude=set()):
     return df.na.fill(stats.first().asDict())
 
 df_ml = fill_missing(df_ml, ["player_id", "position"])
+
+# Convert column types
+df_ml = df_ml.withColumn("week_tweets", df_ml["week_tweets"].cast(IntegerType()))
+df_ml = df_ml.withColumn("efg_pct", df_ml["efg_pct"].cast(DoubleType()))
+df_ml = df_ml.withColumn("efg_pct", F.round(df_ml["efg_pct"], 2))
+
+df_ml = df_ml.drop(df_ml.player_id)
+
+# Convert position column to numerical column after encoding
+indexer = StringIndexer(inputCol='position', outputCol='posNum')
+indexd_data=indexer.fit(df_ml).transform(df_ml)
+
+encoder = OneHotEncoder(inputCol='posNum', outputCol = 'posVec')
+onehotdata = encoder.fit(indexd_data).transform(indexd_data)
+
+# Create feature vector for models
+assembler1 = VectorAssembler(
+ inputCols=["age", "height", "week_tweets", "MPG", "efg_pct", "TRBPG", "APG", "SPG", "BPG", "TOPG", "PPG", "posVec"],
+ outputCol="features")
+outdata1 = assembler1.transform(onehotdata)
+
+df = outdata1.select('features', 'salary')
+df = df.withColumnRenamed("salary","label")
+
+# Save pre-processed data into table with features and label
+cwd = os.getcwd()
+df.repartition(1).write.parquet(cwd+"/processed_data.parquet")
+
